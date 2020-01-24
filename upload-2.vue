09868c431e75ca821fc8a9b67271a19039766f5b
Mert Yildiz <mert.yildiz@orema.com.tr>
2020-01-24 16:17:48 +0300

    updated response code check logic

--- upload-2.vue
+++ upload-2.vue
@@ -839,7 +839,7 @@ export default {
 					if (this.readyState !== 4) {
 						return;
 					}
-					if (this.status === 200 || this.status === 201) {
+					if (this.status >= 200 && this.status <= 400) {
 						resolve(JSON.parse(this.responseText));
 					} else {
 						reject(this.status);
